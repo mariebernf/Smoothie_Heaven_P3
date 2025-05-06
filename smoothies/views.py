5d5ea59dc9be385e747312bc3e4770a853efc633
mariebernf <77eyo7ks@students.codeinstitute.net>
2025-05-07 00:51:44 +0200

    Add bootstrap card layout

--- smoothies/views.py
+++ smoothies/views.py
@@ -1,9 +1,37 @@
 from django.shortcuts import render
 
+smoothies = [
+  {
+
+    'author': 'marie',
+    'name': 'berry blitz',
+    'description': 'refreshing berry smoothie',
+    'ingredients': 'blueberries, stawberries, rasberries,'
+    ' coconut water, honey',
+    'date_posted': 'May 01, 2025',
+},
+{
+    'author': 'marie',
+    'name': 'banana bononza',
+    'description': 'delicious banana smoothie',
+    'ingredients': 'bananas, almond milk, peanut butter, honey',
+    'date_posted': 'May 05, 2025',
+  },
+{
+    'author': 'marie',
+    'name': 'green glow',
+    'description': 'nutritous green smoothie',
+    'ingredients': 'kale, spinach, greek yogurt, pineapple',
+    'date_posted': 'May 07, 2025',
+  }
+]
 
 # Create your views here.
 def home(request):
-    return render(request, 'smoothies/home.html')
+    context = {
+        'smoothies': smoothies
+    }
+    return render(request, 'smoothies/home.html', context)
 
 
 def about(request):
