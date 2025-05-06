dcb3d672503a945ae46836f67acec82aa0b151fa
mariebernf <77eyo7ks@students.codeinstitute.net>
2025-05-06 22:41:42 +0200

    Add home and about views

--- smoothies/views.py
+++ smoothies/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request):
+    return render(request, 'smoothies/home.html')
+
+
+def about(request):
+    return render(request, 'smoothies/about.html')
