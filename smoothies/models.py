97d9c9aba57763b5e85d5faaaf027d9ace860c47
mariebernf <77eyo7ks@students.codeinstitute.net>
2025-05-07 01:20:22 +0200

    Add Smoothie model

--- smoothies/models.py
+++ smoothies/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# Models
+
+class Smoothie(models.Model):
+    name= models.CharField(max_length=100)
+    description = models.TextField()
+    ingredients = models.TextField()
+    author = models.CharField(max_length=50)
+    date_posted = models.DateField(auto_now_add=True)
+
+    
+    def __str__(self):
+       return self.name
