845e4fab4e80aa86f88ae257f3ba6368ecbcf949
mariebernf <77eyo7ks@students.codeinstitute.net>
2025-05-08 00:24:18 +0200

    Create smoothie detail page

--- smoothies/urls.py
+++ smoothies/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+'app/model_viewtype'
+'smoothies/smoothie_detail.html'
+
 urlpatterns = [
-    path('', views.home, name="smoothies-home"),
-    path('about/', views.about, name="about"),
+    path('', views.SmoothieListView.as_view(), name="smoothies-home"),
+    path('<int:pk>/', views.SmoothieDetailView.as_view(), name="smoothies-detail"),
+    path('about/', views.about, name="about"),  
 ]
--- smoothies/views.py
+++ smoothies/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Smoothie 
+from django.views.generic import ListView, DetailView
+from .models import Smoothie
 
 
 def home(request):
@@ -9,6 +10,15 @@ def home(request):
     }
     return render(request, 'smoothies/home.html', context)
 
+class SmoothieListView(ListView):
+    model = Smoothie
+    template_name = 'smoothies/home.html'
+    context_object_name = 'smoothies'
+
+class SmoothieDetailView(DetailView):
+    model = Smoothie
+    template_name = 'smoothies/smoothie_detail.html'
+    context_object_name = 'smoothie'
 
 def about(request):
     return render(request, 'smoothies/about.html')
