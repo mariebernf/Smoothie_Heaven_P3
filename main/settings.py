0468888602a89ff79a9e4c10067f37c9d2aa79d7
mariebernf <77eyo7ks@students.codeinstitute.net>
2025-05-07 16:56:07 +0200

    Add crispy forms for bootstrap5

--- main/settings.py
+++ main/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'smoothies',
     'users',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK= 'bootstrap5'
